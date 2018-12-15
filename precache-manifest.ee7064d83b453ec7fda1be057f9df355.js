78079167d950d7a7ec0e74caa278722b2899e29a
Alexander Arcombe <arcombe@Alexanders-Air.lan>
2018-12-15 09:56:26 +0100

    Updates

---
+++ precache-manifest.ee7064d83b453ec7fda1be057f9df355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bed16a7bfcfc2ada165b",
+    "url": "/arcombe/kungaskogen/static/js/runtime~main.bed16a7b.js"
+  },
+  {
+    "revision": "9cd2e58ad8cca818217a",
+    "url": "/arcombe/kungaskogen/static/js/main.9cd2e58a.chunk.js"
+  },
+  {
+    "revision": "554b7fcb879f2e0c405e",
+    "url": "/arcombe/kungaskogen/static/js/1.554b7fcb.chunk.js"
+  },
+  {
+    "revision": "9cd2e58ad8cca818217a",
+    "url": "/arcombe/kungaskogen/static/css/main.3422ec5f.chunk.css"
+  },
+  {
+    "revision": "9c164df7121eb43ea548fcbace0d2897",
+    "url": "/arcombe/kungaskogen/index.html"
+  }
+];
